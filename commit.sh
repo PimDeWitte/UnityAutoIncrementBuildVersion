e4268c7a408f557e485940ca22d38960915fd2c9
Pim de Witte <pim@whitespell.com>
2016-12-08 23:35:05 -0800

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+ssh-add deploy
 # This always comes from cloud build so, so push with force after the only edited asset is saved(ProjectSettings)
 git commit $1 -m "Automated file edit" && git push --force origin HEAD
 
