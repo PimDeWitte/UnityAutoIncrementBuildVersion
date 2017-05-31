4712fd7b94876c32fc434e5fe8237ec4876fcb98
Pim de Witte <pim@whitespell.com>
2017-05-31 13:55:41 -0700

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
 
-if [ ! -f deploy ]; then
-    echo "UnityAutoIncrementBuildVersion: Deploy File not found! See the documentation on how to create one. Commiting the file back to Unity will likely fail."
-fi
-
-# Add the deploy key, and add github to known hosts so we can push
-ssh-keyscan -H github.com >> ~/.ssh/known_hosts
-ssh-add deploy
 # This always comes from cloud build so, so push with force after the only edited asset is saved(ProjectSettings)
 git commit $1 -m "Automated file edit" && git push --force origin HEAD
 
